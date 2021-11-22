d6ab9c06d63c6803648e03f673a9c4d94115b3b7
polina-g <ignatieva.polina@gmail.com>
2021-11-22 14:25:20 -0500

    adds blogEntry and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class BlogEntry(models.Model):
+    title = models.CharField(max_length=200)
+    blog_text = models.TextField()
+    date_posted = models.DateField()
+    image_url = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    date_posted = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    blog_entry = models.ForeignKey(BlogEntry, on_delete=CASCADE)
+
+    def __str__(self):
+        return f'comment {self.comment_text} by {self.user.name}'
+        
+
+
+
+
