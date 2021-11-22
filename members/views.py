ae15e61139b1504178d394ecae48e9cfdd00e608
alba-molina-nyc <avmanyc@gmail.com>
2021-11-22 18:52:19 -0500

    bare minimum auth complete

--- members/views.py
+++ members/views.py
@@ -3,7 +3,18 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error loging in, please try again") )
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
 
 
 
