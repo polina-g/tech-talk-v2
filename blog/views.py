373d27887f75e47ea77c999616085a37b6674bbe
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-11-22 21:46:43 -0500

    detail page wip

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import BlogEntry
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class BlogIndex(ListView):
+    model = BlogEntry
+    template_name = "blogs/index.html"
