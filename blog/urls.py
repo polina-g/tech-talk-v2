210b8f40bc8b971f428a1c790418092008aa09cd
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-11-24 16:01:36 -0500

    update and delete blog url added

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ path("", views.home, name = "home"),
 path("about/", views.about, name = "about"),
 path("blogs/", views.BlogIndex.as_view(), name = "index"),
 path("blogs/<int:pk>/", views.blogs_detail, name = "detail"),
-path("blogs/<int:pk>/", views.BlogUpdate.as_view(), name = "blog_update"),
-path("blogs/<int:pk>/", views.BlogDelete.as_view(), name = "blog_delete "),
+path("blogs/<int:pk>/update/", views.BlogUpdate.as_view(), name = "blog_update"),
+path("blogs/<int:pk>/", views.BlogDelete.as_view(), name = "blog_delete"),
 
 ]
