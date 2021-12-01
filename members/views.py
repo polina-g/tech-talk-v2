4c21f15a159004d6a88360f5bfea9621b53a8982
alba-molina-nyc <avmanyc@gmail.com>
2021-11-30 20:15:24 -0500

    stopped messing with detail page

--- members/views.py
+++ members/views.py
@@ -25,9 +25,6 @@ class ShowProfilePageView(DetailView):
 def success(request):
     return render('authenticate/success.html')
 
-def profile(request):
-    return render(request, 'authenticate/profile.html')
-
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
